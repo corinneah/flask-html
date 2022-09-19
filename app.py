d45001a02f13d80f38d5b9a7d340f66b41d25538
corinneah <103472876+corinneah@users.noreply.github.com>
2022-09-18 20:39:40 -0400

    update

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+# import packages 
+from flask import Flask, render_template
+import requests 
 
+app = Flask(__name__)
+
+# first page
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+# second page
+@app.route('/other')
+def other_page():
+    return render_template('darksouls.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
